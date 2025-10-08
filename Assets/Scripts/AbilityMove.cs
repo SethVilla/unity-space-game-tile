0010114f11eb6c06bc3dd6aef03451dba9c9f1a1
Seth Villa <sethvilla13@gmail.com>
2025-10-08 11:22:55 -0700

    update default move speed for player

--- Assets/Scripts/AbilityMove.cs
+++ Assets/Scripts/AbilityMove.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class AbilityMove : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveSpeed = 2f;
     
     // Extra distance before destroying object
     private Camera mainCamera;
